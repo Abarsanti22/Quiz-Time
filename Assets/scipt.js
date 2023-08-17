4c3c699e46ef6f8dd27fce206554c868b0aeabf0
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-08-17 17:55:27 -0400

    updated JS

---
+++ Assets/scipt.js
@@ -0,0 +1,33 @@
+var intro =document.querySelector("#intro");
+var startquiz =document.querySelector("#start-quiz");
+var startbtn =document.querySelector("#start");
+
+var questpage =document.querySelector("#questions-page");
+var question =document.querySelector("#question");
+
+var buttons= document.querySelector(".choices");
+var answer1= document.querySelector("#answer1");
+var answer2= document.querySelector("#answer2");
+var answer3= document.querySelector("#answer3");
+var answer4= document.querySelector("#answer4");
+
+var checkanswer= document.querySelector("#check-answer");
+var initial= document.querySelector("#initals");
+var score= document.querySelector("#submit-page");
+var finalscore= document.querySelector("#final");
+
+var submit= document.querySelector("#submit-btn");
+var highscores= document.querySelector("#high-scores");
+var finish= document.querySelector("#finish");
+// var checkscore= document.querySelector("#check-answer");
+// var leaderboard= document.querySelector("#check-answer");
+var restart= document.querySelector("#restart");
+var clear= document.querySelector("#clear");
+
+var questionTime = [
+    {
+        question: "What are the large rocks that orbit the sun between Mars and Jupiter called?",
+        options: ["A. Meteorites", "B. Planets", "C.Asteroids", "D"]
+    }
+]
+
