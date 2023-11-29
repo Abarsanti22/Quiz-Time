f924c272f37e6bfb3e784fc429c8fbc020364168
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-11-29 15:26:33 -0500

    added questions

---
+++ questions.js
@@ -0,0 +1,43 @@
+const quizQuestions = [
+    {
+        question: "What are the large rocks that orbit the sun between Mars and Jupiter called?",
+        choice1: "Meterorites",
+        choice2: "Planets",
+        choice3: "Asteroids",
+        choice4: "Large rocks",
+        correctAnswer: "3"
+
+    },
+    {
+        question: "Who invented the light bulb?",
+        choice1: "Albert Einstein",
+        choice2: "Thomas Edison",
+        choice3: "Alexander Graham Bell",
+        choice4: "Eddie Lighthouse",
+        correctAnswer: "2"
+    },
+    {
+        question: "What is the plural form of the word deer?",
+        choice1: "deer",
+        choice2: "deerses",
+        choice3: "deers",
+        choice4: "dose",
+        correctAnswer: "1"
+    },
+    {
+        question: "How many inches are in a foot?",
+        choice1: "12",
+        choice2: "11",
+        choice3: "24",
+        choice4: "2",
+        correctAnswer: "1",
+    },
+    {
+        question: "What is the capital of New York?",
+        choice1: "New York City",
+        choice2: "Buffalo",
+        choice3: "Rochester",
+        choice4: "Albany",
+        correctAnswer: "4"
+    },
+]
