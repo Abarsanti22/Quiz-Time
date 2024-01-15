942776fc8a1d6adb69e653c7de5df54caa35a6d8
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2024-01-14 20:07:38 -0500

    updated questions format

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,43 +1,32 @@
-const quizQuestions = [
-    {
-        question: "What are the large rocks that orbit the sun between Mars and Jupiter called?",
-        choice1: "Meterorites",
-        choice2: "Planets",
-        choice3: "Asteroids",
-        choice4: "Large rocks",
-        correctAnswer: "3"
 
+var questions = [
+    {
+      title: 'What are the large rocks that orbit the sun between Mars and Jupiter called?',
+      choices: ['Meterorites', 'Planets', 'Asteroids', 'Large rocks'],
+      answer: 'Asteroids',
     },
     {
-        question: "Who invented the light bulb?",
-        choice1: "Albert Einstein",
-        choice2: "Thomas Edison",
-        choice3: "Alexander Graham Bell",
-        choice4: "Eddie Lighthouse",
-        correctAnswer: "2"
+      title: 'Who invented the light bulb?',
+      choices: ['Albert Einstein', 'Thomas Edison', 'Alexander Graham Bell', 'Eddie Lighthouse'],
+      answer: 'Thomas Edison',
     },
     {
-        question: "What is the plural form of the word deer?",
-        choice1: "deer",
-        choice2: "deerses",
-        choice3: "deers",
-        choice4: "dose",
-        correctAnswer: "1"
+      title: 'What is the plural form of the word deer?',
+      choices: [
+        'deer','deerses','deers','dose',],
+      answer: 'deer',
     },
     {
-        question: "How many inches are in a foot?",
-        choice1: "12",
-        choice2: "11",
-        choice3: "24",
-        choice4: "2",
-        correctAnswer: "1",
+      title:
+        'How many inches are in a foot?',
+      choices: ['10', '12', '11', '24'],
+      answer: '12',
     },
     {
-        question: "What is the capital of New York?",
-        choice1: "New York City",
-        choice2: "Buffalo",
-        choice3: "Rochester",
-        choice4: "Albany",
-        correctAnswer: "4"
+      title:
+        'What is the capital of New York?',
+      choices: ['New York City', 'Albany', 'Rochester', 'Buffalo'],
+      answer: 'Albany',
     },
-]
+  ];
+  
